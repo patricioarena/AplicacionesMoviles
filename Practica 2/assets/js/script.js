973db109fd8d4d9df389e0bf2f57f01d82efe35c
Ayelen Palmieri <ayelenpalmieri9012@gmail.com>
2020-09-24 01:26:30 -0300

    feat: modificacion de funcion jquery

--- Practica 2/assets/js/script.js
+++ Practica 2/assets/js/script.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("mailto").addEventListener('click', mailto)
+    document.getElementById("mailto").addEventListener('click', mailto);
+    styloBox();
+    suma();
 });
 
 
@@ -9,3 +11,44 @@ function mailto() {
         + "&body=" + encodeURIComponent("Razon de contacto");
 }
 
+function styloBox() {
+    $('.item').on('click', function() {
+        var id = $(this).attr("id");
+        $(`#${id}`).toggleClass("seleccionado"); 
+        $(this).children(".box").toggleClass("imgopacity");
+        var op = $(this).attr("isSelected");
+        if(op == "true"){
+            $(this).attr("isSelected","false");
+        }else{
+            $(this).attr("isSelected","true");
+        }
+    });
+}
+
+function suma(){
+    $('.item').on('click', function() {
+        var array = [];
+        $('.item').each(function() {
+            var op = $(this).attr("isSelected");
+            if(op == "true"){
+                array.push(this);
+                console.log(array);
+            }
+        })
+        var array2 = []
+        $.each(array, function(index, elem) {
+            array2.push(elem.value($`{span}`));
+            console.log(array2);
+        })
+        var obtenerValor = $('.precioProducto').attr("span");
+        // $('.precioProducto').each(function(){
+        //     sessionStorage.setItem("variableAcumuladora", variableAcumuladora + Number(obtenerValor));
+        // });
+
+        // variableAcumuladora = sessionStorage.getItem("variableAcumuladora");
+        // capturaAcumulable = Number(variableAcumuladora)
+        // console.log(capturaAcumulable);
+        // console.log(obtenerValor)
+        })
+    };
+    
