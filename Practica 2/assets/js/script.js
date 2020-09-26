877dec66ffd87ca59f5decca5c53e26212e55f48
Patricio Arena <patricio.e.arena@gmail.com>
2020-09-25 21:38:13 -0300

    feat: añadido carrito de compras

--- Practica 2/assets/js/script.js
+++ Practica 2/assets/js/script.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     document.getElementById("mailto").addEventListener('click', mailto);
+    document.getElementById("bag").addEventListener('click', openNav);
+    document.getElementById("Sidebar-bag").addEventListener('click', closeNav);
+    document.getElementById("confirVenta").addEventListener('click', Confirmar);
+
     styloBox();
     suma();
+
 });
 
+hayProductos = 0;
 
 function mailto() {
     document.location.href = "mailto:mypetshop2020@gmail.com?subject="
@@ -12,61 +19,95 @@ function mailto() {
 }
 
 function styloBox() {
-    $('.item').on('click', function() {
+    $('.item').on('click', function () {
         var id = $(this).attr("id");
         $(`#${id}`).toggleClass("seleccionado");
         $(this).children(".box").toggleClass("imgopacity");
         var op = $(this).attr("isSelected");
-        if(op == "true"){
-            $(this).attr("isSelected","false");
-        }else{
-            $(this).attr("isSelected","true");
+        if (op == "true") {
+            $(this).attr("isSelected", "false");
+        } else {
+            $(this).attr("isSelected", "true");
         }
     });
 }
 
-function suma(){
+function suma() {
     var storage = localStorage.getItem("productosAcumulados");
     console.log(storage);
     var arrayAux = [];
-    if (storage != null){
+
+    if (storage != null) {
         arrayAux = storage;
     }
+
     $(document).on('click', '.item', function () {
         var arrayProductos = [];
-        $('.item').each(function() {
+        $('.item').each(function () {
             var op = $(this).attr("isSelected");
-            if(op == "true"){
+            if (op == "true") {
                 arrayProductos.push(this);
             }
         })
+
+        $('#tabla').empty()
+        
+        let temp = $('#tabla').text().trim();
+        if (temp == "") {
+            document.getElementById("total").innerText = 0;
+        }
+
         var arrayPrecioProductos = []
-        $.each(arrayProductos, function(index, elem) {
+        $.each(arrayProductos, function (index, elem) {
+            var titulo = $(elem).children('.tituloProducto').text().trim();
             var precioOriginal = $(elem).children('.precioProducto').text();
-            var precioRegular = precioOriginal.replace(/[$.]/g,'');
+            var precioRegular = precioOriginal.replace(/[$.]/g, '');
             var precioSinEspacios = precioRegular.trim();
             var precioParseInt = parseInt(precioSinEspacios);
             arrayPrecioProductos.push(precioParseInt);
+
+            let x = `
+            <tr>
+            <th>${titulo}</th>
+                <th>${precioRegular}</th>
+            </tr>`
+
+            $("#tabla").append($(x));
+
+            sum = 0;
+            $.each(arrayPrecioProductos, function (index, value) {
+                sum += value;
+            });
+
+            document.getElementById("bagCounter").innerText = arrayPrecioProductos.length;
+            document.getElementById("total").innerText = sum;
+
+
         })
+    })
 
-        sum = 0;
-        $.each(arrayPrecioProductos, function (index, value) {
-            sum += value;
-        });
 
-        console.log(sum);
 
-        localStorage.setItem("productosAcumulados", arrayProductos);
 
-        //var obtenerValor = $('.precioProducto').attr("span");
-        // $('.precioProducto').each(function(){
-        //     sessionStorage.setItem("variableAcumuladora", variableAcumuladora + Number(obtenerValor));
-        // });
+};
 
-        // variableAcumuladora = sessionStorage.getItem("variableAcumuladora");
-        // capturaAcumulable = Number(variableAcumuladora)
-        // console.log(capturaAcumulable);
-        // console.log(obtenerValor)
-        })
-    };
+function openNav() {
+    document.getElementById("Sidebar-bag").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+    document.getElementById("price").style.width = "250px";
+}
 
+function closeNav() {
+    document.getElementById("Sidebar-bag").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+    document.getElementById("price").style.width = "0";
+}
+
+function Confirmar() {
+    let temp = $('#tabla').text().trim();
+    if (temp == "") {
+        alert('No se puede concretar la venta')
+    } else {
+        alert('Venta concretada')
+    }
+}
