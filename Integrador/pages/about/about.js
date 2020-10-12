10bf04571cb9403ef63a7d7433d217b4492ae18a
Ayelen Palmieri <ayelenpalmieri9012@gmail.com>
2020-10-11 21:17:57 -0300

    feat map

--- Integrador/pages/about/about.js
+++ Integrador/pages/about/about.js
@@ -11,15 +11,6 @@ function back() {
 }
 
 function initMap(){
-    // var coordenadas = {lat: -34.9228288 ,lng: -57.9562555};
-    // var map = new google.maps.Map(document.getElementById('map'),{
-    //     zoom: 10,
-    //     center: coordenadas
-    // });
-    // var marker = new google.maps.Marker({
-    //     position: coordenadas,
-    //     map: map
-    // });
     navigator.geolocation.getCurrentPosition(function_ok, function_error);
 }
 
@@ -30,43 +21,75 @@ function initMap(){
 // }
 
 function function_ok(respuesta){
-    //mostrarObjeto(respuesta.coords);
-    var divMapa = document.getElementById('map');
+    var titlesProvider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     var latitud = respuesta.coords.latitude;
     var longitud = respuesta.coords.longitude;
-    //divMapa.innerHTML = latitud+','+longitud;
-    // divMapa.innerHTML = "Tenemos autorizacion para ver su ubicacion";
 
-    var gLatLon = new google.maps.LatLng(latitud,longitud);
-    var objConfig = {
-        zoom : 17,
-        center : gLatLon
-    }
-    var gMapa = new google.maps.Map(divMapa, objConfig);
-    var objConfigMarker = {
-        position : gLatLon,
-        map : gMapa,
-        title : "Usted esta Aquí"
-    }
-    var gMarker = new google.maps.Marker(objConfigMarker);
+    var gLatLon = L.map('map').setView([latitud, longitud], 10);
 
-    var gCoder = new google.maps.Geocoder();
-    var objInformacion = {
-        address : 'Calle 14, B1900 La Plata, Provincia de Buenos Aires'
-    }
-    gCoder.geocode(objInformacion, function_coder);
+    L.tileLayer(titlesProvider, {
+        maxZoom: 18,
+    }).addTo(gLatLon)
 
-    function function_coder(datos){
-        var coordenadas = datos[0].geometry.location; //obj LatLong
-        var config = {
-            map: gMapa,
-            position: coordenadas,
-            title: 'Catedral de La Plata'
+
+    // var gLatLon = new google.maps.LatLng(latitud,longitud);
+    // var objConfig = {
+    //     zoom : 17,
+    //     center : gLatLon
+    // }
+
+    var gMarker = L.marker([latitud, longitud]);
+    gMarker.addTo(gLatLon);
+
+    var iconMarker = L.icon({
+        iconUrl : '../../assets/img/tienda.png',
+        iconSize : [60, 60],
+        iconAnchor : [30, 60]
+    })
+
+    var positionMarket = [-34.9251137, -57.9540854];
+    var gMarker2 = L.marker(positionMarket, {icon: iconMarker});
+    gMarker2.addTo(gLatLon);
+
+    document.getElementById('select-location').addEventListener('change', function(e){
+        let value = e.target.value;
+        if (value == "myPosition"){
+            let myCoords = [latitud, longitud];
+            console.log(myCoords);
+            gLatLon.flyTo(myCoords,18);
+        }else{
+            console.log(positionMarket);
+            gLatLon.flyTo(positionMarket,18);
         }
-        var gMarkerDV = new google.maps.Marker(config);
-        //gMarkerDV.setIcon('catedral-la-plata.png');
+    })
 
-    }
+
+    // var gMapa = new google.maps.Map(divMapa, objConfig);
+    // var objConfigMarker = {
+    //     position : gLatLon,
+    //     map : gMapa,
+    //     title : "Usted esta Aquí"
+    // }
+    // var gMarker = new google.maps.Marker(objConfigMarker);
+
+
+    // var gCoder = new google.maps.Geocoder();
+    // var objInformacion = {
+    //     address : 'Calle 14, B1900 La Plata, Provincia de Buenos Aires'
+    // }
+    // gCoder.geocode(objInformacion, function_coder);
+
+    // function function_coder(datos){
+    //     var coordenadas = datos[0].geometry.location; //obj LatLong
+    //     var config = {
+    //         map: gMapa,
+    //         position: coordenadas,
+    //         title: 'Catedral de La Plata'
+    //     }
+    //     var gMarkerDV = new google.maps.Marker(config);
+    //     //gMarkerDV.setIcon('catedral-la-plata.png');
+
+    // }
 }
 
 function function_error(){
