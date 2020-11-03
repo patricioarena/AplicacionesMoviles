6b26b9fb593200cd63c410aabca29c1cd7fea896
Patricio Arena <patricio.e.arena@gmail.com>
2020-11-03 17:00:16 -0300

    feat: MaestroDetalle funcionando

--- MaestroDetalle/app/src/main/java/com/example/maestrodetalle/ItemDetailFragment.kt
+++ MaestroDetalle/app/src/main/java/com/example/maestrodetalle/ItemDetailFragment.kt
@@ -1,13 +1,18 @@
 package com.example.maestrodetalle
 
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import com.google.android.material.appbar.CollapsingToolbarLayout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.example.maestrodetalle.dummy.DummyContent
+import com.google.android.material.appbar.CollapsingToolbarLayout
+
 
 /**
  * A fragment representing a single Item detail screen.
@@ -20,7 +25,9 @@ class ItemDetailFragment : Fragment() {
     /**
      * The dummy content this fragment is presenting.
      */
+
     private var item: DummyContent.DummyItem? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,15 +41,31 @@ class ItemDetailFragment : Fragment() {
                 activity?.findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout)?.title = item?.content
             }
         }
+
     }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.item_detail, container, false)
 
+
         // Show the dummy content as text in a TextView.
         item?.let {
-            rootView.findViewById<TextView>(R.id.item_detail).text = it.details
+//            rootView.findViewById<TextView>(R.id.item_detail).text = it.details
+            val webView: WebView = rootView.findViewById(R.id.item_detail)
+
+            webView.webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(
+                    view: WebView, request: WebResourceRequest
+                ): Boolean {
+                    return super.shouldOverrideUrlLoading(
+                        view, request)
+                }
+            }
+            webView.settings.javaScriptEnabled = true
+            webView.loadUrl(item?.details.toString())
+
         }
 
         return rootView
--- MaestroDetalle/app/src/main/java/com/example/maestrodetalle/dummy/DummyContent.kt
+++ MaestroDetalle/app/src/main/java/com/example/maestrodetalle/dummy/DummyContent.kt
@@ -21,13 +21,17 @@ object DummyContent {
      */
     val ITEM_MAP: MutableMap<String, DummyItem> = HashMap()
 
-    private val COUNT = 25
+//    private val COUNT = 3
 
     init {
         // Add some sample items.
-        for (i in 1..COUNT) {
-            addItem(createDummyItem(i))
-        }
+//        for (i in 0..COUNT) {
+//             addItem(createDummyItem(i))
+//        }
+        addItem(createDummyItem(0, "Hide"))
+        addItem(createDummyItem(1, "Amazon"))
+        addItem(createDummyItem(2, "Google"))
+        addItem(createDummyItem(3, "Android"))
     }
 
     private fun addItem(item: DummyItem) {
@@ -35,17 +39,31 @@ object DummyContent {
         ITEM_MAP.put(item.id, item)
     }
 
-    private fun createDummyItem(position: Int): DummyItem {
-        return DummyItem(position.toString(), "Item " + position, makeDetails(position))
+//    private fun createDummyItem(position: Int): DummyItem {
+//        return DummyItem(position.toString(), "Item " + position, makeDetails(position))
+//    }
+
+    private fun createDummyItem(position: Int, name: String): DummyItem {
+        return DummyItem(position.toString(), name, makeDetails(position))
     }
 
-    private fun makeDetails(position: Int): String {
-        val builder = StringBuilder()
-        builder.append("Details about Item: ").append(position)
-        for (i in 0..position - 1) {
-            builder.append("\nMore details information here.")
+//    private fun makeDetails(position: Int): String {
+//        val builder = StringBuilder()
+//        builder.append("Details about Item: ").append(position)
+//        for (i in 0..position - 1) {
+//            builder.append("\nMore details information here.")
+//        }
+//        return builder.toString()
+//    }
+
+    private fun makeDetails(position: Int) : String{
+        var url = ""
+        when (position){
+            1 -> url = "https://www.amazon.com/-/es/";
+            2 -> url = "https://www.google.com/";
+            3 -> url = "https://developer.android.com/studio";
         }
-        return builder.toString()
+        return url
     }
 
     /**
