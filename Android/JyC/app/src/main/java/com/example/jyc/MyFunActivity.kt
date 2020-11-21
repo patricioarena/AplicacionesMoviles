d1ab82e3ae7b64d990954ac002708dfaa2a03702
Patricio Arena <patricio.e.arena@gmail.com>
2020-11-21 03:01:28 -0300

    feat: funcionalidad de nueva publicacion, almacenamiento en base de datos y maquetado de modelo de datos para publicaciones

--- Android/JyC/app/src/main/java/com/example/jyc/MyFunActivity.kt
+++ Android/JyC/app/src/main/java/com/example/jyc/MyFunActivity.kt
@@ -2,72 +2,32 @@ package com.example.jyc
 
 import MyResources.Facade
 import android.content.Intent
-
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.appcompat.widget.Toolbar;
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_home.*
-import java.util.*
+import androidx.appcompat.widget.Toolbar
 
-
-class HomeActivity : AppCompatActivity() {
-    private var pressedTime: Long = 0
-    private lateinit var service: Facade
+class MyFunActivity : AppCompatActivity() {
     private lateinit var toolbar: Toolbar
-
+    private lateinit var service: Facade
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-
-        service = Facade()
+        setContentView(R.layout.activity_my_fun)
 
         // Agregar toolbar personalizado a activity main
         toolbar = findViewById(R.id.myToolbar)
-        toolbar.title = "HomeActivity";
-        toolbar.subtitle = "Contenido principal del feed";
+        toolbar.title = "FuntionActivity";
+        toolbar.subtitle = "Test functions DEV";
+
         setSupportActionBar(toolbar)
-
-        val post = Post("Hola mundo", Date(), "Ayelen")
-        val post2 = Post("Hola mundo", Date(), "Esteban Quito")
-        val post3 = Post("Hola mundo", Date(), "Patricio")
-        val post4 = Post("Hola mundo", Date(), "Ayelen")
-        val post5 = Post("Hola mundo", Date(), "Armando Esteban Quito")
-
-        val posts = listOf(post,post2,post3,post4,post5)
-
-        recyclerView.apply {
-            setHasFixedSize(true)
-            layoutManager = LinearLayoutManager(this@HomeActivity)
-            adapter = PostAdapter(this@HomeActivity, posts)
-        }
-
+        service = Facade()
     }
 
-
-    // Si la autenticacion es satisfactoria guardamos el token
-    // por lo tanto la implementacion de la funcionalidad de logout
-    // se basa en el paso inverso que es borrar el token y posteriormente cerrar la aplicacion
-    // tambien podriamos reenviar al usuario al login
     private fun logoutUser() {
         service.deletePreferenceKey(this, "token")
         super.finishAffinity()
-    }
-
-    // Sobrescribimos el metodo que la salida de la aplicacion sea mas agil
-    // de no ser asi volverimos a la actividad Splash y tendriamos que salir desde ahi
-    override fun onBackPressed() {
-
-        if (pressedTime + 2000 > System.currentTimeMillis()) {
-            super.finishAffinity()
-        } else {
-            Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show();
-        }
-
-        pressedTime = System.currentTimeMillis();
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -76,7 +36,7 @@ class HomeActivity : AppCompatActivity() {
         inflater.inflate(R.menu.menu_toobar, menu)
         return super.onCreateOptionsMenu(menu)
     }
-
+    
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle presses on the action bar menu items
         when (item.itemId) {
@@ -107,6 +67,4 @@ class HomeActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
-
-
 }
--- Android/JyC/app/src/main/java/com/example/jyc/PublicationActivity.kt
+++ Android/JyC/app/src/main/java/com/example/jyc/PublicationActivity.kt
@@ -2,71 +2,69 @@ package com.example.jyc
 
 import MyResources.Facade
 import android.Manifest
-import android.content.ContentResolver
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.WindowManager
-import android.webkit.MimeTypeMap
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import java.io.ByteArrayOutputStream
 import java.util.*
 
 
-class FunctionsActivity : AppCompatActivity() {
+class PublicationActivity : AppCompatActivity() {
     private lateinit var toolbar: Toolbar
     private lateinit var service: Facade
     private lateinit var textView6: TextView
-
+    private lateinit var editTextTextMultiLine: EditText
     private lateinit var button_1: Button
     private lateinit var button_2: Button
     private lateinit var auth: FirebaseAuth
-
+    private lateinit var database: FirebaseFirestore
     private var mStorageRef: StorageReference? = null
     private var imageView: ImageView? = null
     private var bitmap: Bitmap? = null
-    var currentPhotoPath: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_functions)
+        setContentView(R.layout.activity_publication)
 
         service = Facade()
 
         // Agregar toolbar personalizado a activity main
         toolbar = findViewById(R.id.myToolbar)
-        toolbar.title = "FuntionActivity";
-        toolbar.subtitle = "Test functions DEV";
+        toolbar.title = "Nueva publication";
+        toolbar.subtitle = "My super publication"
 
         setSupportActionBar(toolbar)
+
         auth = FirebaseAuth.getInstance()
         mStorageRef = FirebaseStorage.getInstance().getReference();
+        database = FirebaseFirestore.getInstance()
 
         button_1 = findViewById(R.id.button_1)
         button_2 = findViewById(R.id.button_2)
 
         textView6 = findViewById(R.id.textView6)
+        editTextTextMultiLine = findViewById(R.id.editTextTextMultiLine)
+
         imageView = findViewById(R.id.image)
 
         button_1.isEnabled = false
         button_1.text = "Take pic"
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 111)
         } else {
             button_1.isEnabled = true
@@ -112,7 +110,11 @@ class FunctionsActivity : AppCompatActivity() {
                 return true
             }
             R.id.nav_testActivity -> {
-                startActivity(Intent(this, FunctionsActivity::class.java))
+                startActivity(Intent(this, MyFunActivity::class.java))
+                return true
+            }
+            R.id.nav_new_pub -> {
+                startActivity(Intent(this, PublicationActivity::class.java))
                 return true
             }
             R.id.nav_logout -> {
@@ -124,9 +126,9 @@ class FunctionsActivity : AppCompatActivity() {
     }
 
     override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -144,33 +146,56 @@ class FunctionsActivity : AppCompatActivity() {
 
     private fun upload() {
         window.setFlags(
-            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
-            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
         )
+        val user: FirebaseUser? = auth.currentUser
+        var idUsuario = user?.uid.toString()
+
         val stream = ByteArrayOutputStream()
         bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, stream)
         val random: String = UUID.randomUUID().toString()
-        val imageRef: StorageReference? = mStorageRef?.child("image/$random")
+        val imageRef: StorageReference? = mStorageRef?.child("$idUsuario/$random")
 
         val b: ByteArray = stream.toByteArray()
         if (imageRef != null) {
             imageRef.putBytes(b)
-                .addOnSuccessListener { taskSnapshot ->
-                    window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
-                    taskSnapshot.metadata!!.reference!!.downloadUrl.addOnSuccessListener { uri ->
-                        val downloadUri = uri
-                        textView6.text = downloadUri.toString()
+                    .addOnSuccessListener { taskSnapshot ->
+                        window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+                        taskSnapshot.metadata!!.reference!!.downloadUrl.addOnSuccessListener { uri ->
+                            val downloadUri = uri
+                            var imagenUri = downloadUri.toString()
+                            textView6.text = imagenUri
+
+                            var emptyStringArray = listOf("")
+                            var categorias = emptyStringArray
+                            var tags = emptyStringArray
+                            var comentarios = emptyStringArray
+                            var fecha = service.getDateTime()
+                            var textomuylargo = editTextTextMultiLine.text.toString()
+
+                            var publicacion = hashMapOf(
+                                    "idUsuario" to idUsuario,
+                                    "articulo" to textomuylargo,
+                                    "fecha" to fecha,
+                                    "imagen" to imagenUri,
+                                    "categorias" to categorias,
+                                    "tags" to tags,
+                                    "comentarios" to comentarios
+                            )
+
+                            val users = database.collection("publicaciones")
+                            users.document().set(publicacion)
+
+                        }
+                        Toast.makeText(this, "Photo Uploaded", Toast.LENGTH_SHORT).show();
                     }
-                    Toast.makeText(this, "Photo Uploaded", Toast.LENGTH_SHORT).show();
-                }
-                .addOnFailureListener {
-                    window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
-                    Toast.makeText(this, "Upload Failed", Toast.LENGTH_SHORT).show();
-                }
+                    .addOnFailureListener {
+                        window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+                        Toast.makeText(this, "Upload Failed", Toast.LENGTH_SHORT).show();
+                    }
         }
     }
-
-
 
 
 }
