e716318501443ed4c9e01fd5f67676e1d4eb3771
Ayelen Palmieri <ayelenpalmieri9012@gmail.com>
2020-11-28 23:48:42 -0300

    xml de profile user y edit user

---
+++ Android/JyC/app/src/main/java/com/example/jyc/ProfileActivity.kt
@@ -0,0 +1,19 @@
+package com.example.jyc
+
+import MyResources.Facade
+import android.content.ContentValues
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QuerySnapshot
+import kotlinx.android.synthetic.main.activity_home.*
+
+class ProfileActivity : AppCompatActivity() {
+   
+}
